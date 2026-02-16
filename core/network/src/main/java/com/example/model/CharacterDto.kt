1b828799f47b440b5dee81c1d0c46417c4b3dc53
meeten <153901120+meeten@users.noreply.github.com>
2026-02-16 19:48:19 +0200

    Add Retrofit, OkHttp, DTOs and API services

---
+++ core/network/src/main/java/com/example/model/CharacterDto.kt
@@ -0,0 +1,20 @@
+package com.example.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CharacterDto(
+    @SerializedName("gender")
+    val gender: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("species")
+    val species: String,
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("type")
+    val type: String,
+    @SerializedName("image")
+    val imageUrl: String
+)
