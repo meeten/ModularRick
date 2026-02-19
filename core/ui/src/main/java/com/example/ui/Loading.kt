a7a0092e51a8d33411f66e62b5e561a2d603054a
meeten <153901120+meeten@users.noreply.github.com>
2026-02-19 20:30:21 +0200

    Implement Loading indicator in core module

---
+++ core/ui/src/main/java/com/example/ui/Loading.kt
@@ -0,0 +1,19 @@
+package com.example.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun Loading(modifier: Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize(),
+    ) {
+        CircularProgressIndicator()
+    }
+}
