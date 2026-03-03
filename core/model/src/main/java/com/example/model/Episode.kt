e3b73079d85542b538c4ea00bfc643f8c8dad5ee
meeten <153901120+meeten@users.noreply.github.com>
2026-03-03 19:24:18 +0200

    Replace episode string with season and episode numbers

--- core/model/src/main/java/com/example/model/Episode.kt
+++ core/model/src/main/java/com/example/model/Episode.kt
@@ -4,6 +4,7 @@ data class Episode(
     val id: Int,
     val name: String,
     val airDate: String,
-    val episode: String,
+    val seasonNumber: Int,
+    val episodeNumber: Int,
     val characters: List<String>
 )
