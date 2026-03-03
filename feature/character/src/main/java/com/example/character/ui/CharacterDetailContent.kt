cef12dd3d8e36cdfab41ea141e93de96006078ce
meeten <153901120+meeten@users.noreply.github.com>
2026-03-03 19:47:57 +0200

    Add missing imports

--- feature/character/src/main/java/com/example/character/ui/CharacterDetailContent.kt
+++ feature/character/src/main/java/com/example/character/ui/CharacterDetailContent.kt
@@ -8,14 +8,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
