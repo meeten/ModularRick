058d73ae392b440714d45e10f243fcf00802ec7b
meeten <153901120+meeten@users.noreply.github.com>
2026-03-13 21:36:19 +0200

    Add MockWebServer dependency and test resources

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -34,6 +34,14 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 dependencies {
@@ -44,16 +52,21 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
-    // hilt
+    //hilt
     implementation("com.google.dagger:hilt-android:2.57.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    implementation(libs.core.ktx)
     ksp("com.google.dagger:hilt-compiler:2.57.1")
+
+    //mockwebsever
+    testImplementation("com.squareup.okhttp3:mockwebserver:5.3.2")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.core.ktx)
     testImplementation(libs.junit)
+    testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
+    testRuntimeOnly(libs.junit.platform.launcher)
     androidTestImplementation(libs.androidx.espresso.core)
 }
