f42d4809f5e625e0f6842bc7a1c392fdbf6bfe67
meeten <153901120+meeten@users.noreply.github.com>
2026-03-20 22:34:09 +0200

    Remove unused pages field from InfoDto

--- core/network/src/main/java/com/example/model/InfoDto.kt
+++ core/network/src/main/java/com/example/model/InfoDto.kt
@@ -3,6 +3,5 @@ package com.example.model
 import com.google.gson.annotations.SerializedName
 
 data class InfoDto(
-    @SerializedName("pages") val pages: Int,
     @SerializedName("next") val nextPageUrl: String?
 )
